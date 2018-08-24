bb2a15609878fcd0ed5f0b6cb758eb0d740d7694
SURAMERICANA\carldihe <carlos.diaz@ceiba.com.co>
2018-08-24 11:07:26 -0500

    introduccion node

---
+++ 03-bases-node/app.js
@@ -0,0 +1,28 @@
+const argv = require('yargs')
+    .command('listar', 'imprime en consola la tabla de multiplicar', {
+        base: {
+            demand: true,
+            alias: 'b'
+        },
+        limite: {
+            alias: 'l',
+            default: 10
+        }
+    })
+    .help()
+    .argv;
+
+const { crearArchivo } = require('./multiplicar/multiplcar');
+
+let argv2 = process.argv;
+
+console.log(argv.base);
+console.log(argv.limite);
+
+// let parametro = argv[2];
+// let base = parametro.split('=')[1]
+
+
+// crearArchivo(base)
+//     .then(archivo => console.log(`archivo creado: ${archivo}`))
+//     .catch(e => console.log(e));
