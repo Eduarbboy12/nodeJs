667ce98f73b06b6d7b2107effb09a9feeb7dec47
SURAMERICANA\carldihe <carlos.diaz@ceiba.com.co>
2018-09-04 09:48:34 -0500

    restServer

--- 07-restserver/server/config/config.js
+++ 07-restserver/server/config/config.js
@@ -1,4 +1,23 @@
-/**
- * Puero
- */
+// =======================
+// Puerto
+// =======================
 process.env.PORT = process.env.PORT || 3000;
+
+// =======================
+// Ambiente
+// =======================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+// =======================
+// Base de Datos
+// =======================
+
+let urlDB;
+
+if (process.env.NODE_ENV == 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+    urlDB = 'mongodb://eduarbboy12:abc123@ds145072.mlab.com:45072/cafe'
+}
+
+process.env.URLDB = urlDB;
--- 07-restserver/server/server.js
+++ 07-restserver/server/server.js
@@ -1,9 +1,10 @@
 require('./config/config');
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
 
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -11,40 +12,14 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuario', (req, res) => {
-    res.json('getUsuario')
+app.use(require('./routes/usuario'));
+
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true }, (err) => {
+    if (err) throw err;
+
+    console.log('Base de datos ONLINE');
 });
-
-app.post('/usuario', (req, res) => {
-
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-});
-
-app.put('/usuario', (req, res) => {
-    res.json('putUsuario')
-});
-
-app.put('/usuario/:id', (req, res) => {
-    let id = req.params.id;
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', (req, res) => {
-    res.json('deleteUsuario')
-});
+mongoose.set('useCreateIndex', true)
 
 app.listen(process.env.PORT, () => {
     console.log(`Escuchando puerto: `, process.env.PORT);
